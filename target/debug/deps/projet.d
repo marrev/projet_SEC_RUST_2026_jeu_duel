4d3df71b0eb57212d9605f83149dec0b641f1887
marrev <julien.depeyris@laposte.net>
2026-03-27 15:24:54 +0100

    Init folder with nano

---
+++ target/debug/deps/projet.d
@@ -0,0 +1,5 @@
+C:\CentraleNantes\SEC3\Rust\projet_SEC_RUST_2026_jeu_duel\projet\target\debug\deps\projet.d: src\main.rs
+
+C:\CentraleNantes\SEC3\Rust\projet_SEC_RUST_2026_jeu_duel\projet\target\debug\deps\projet.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/projet.d
@@ -0,0 +1,1 @@
+C:\CentraleNantes\SEC3\Rust\projet_SEC_RUST_2026_jeu_duel\target\debug\projet.exe: C:\CentraleNantes\SEC3\Rust\projet_SEC_RUST_2026_jeu_duel\src\main.rs
